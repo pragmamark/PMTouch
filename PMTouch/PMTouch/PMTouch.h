59ccd68bf0eaf8aa391df5cc3ec03e90ee00e23d
Massimo Oliviero <massimo.oliviero@gmail.com>
2012-05-22 22:19:40 +0200

    added xcode empty project

---
+++ PMTouch/PMTouch/PMTouch.h
@@ -0,0 +1,13 @@
+//
+//  PMTouch.h
+//  PMTouch
+//
+//  Created by Massimo Oliviero on 5/22/12.
+//  Copyright (c) 2012 pragmamark.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMTouch : NSObject
+
+@end
---
+++ PMTouch/PMTouch/PMTouch.m
@@ -0,0 +1,13 @@
+//
+//  PMTouch.m
+//  PMTouch
+//
+//  Created by Massimo Oliviero on 5/22/12.
+//  Copyright (c) 2012 pragmamark.org. All rights reserved.
+//
+
+#import "PMTouch.h"
+
+@implementation PMTouch
+
+@end
